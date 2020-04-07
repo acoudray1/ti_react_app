d918589f5dc09a709cf27943c899992ce957beae
axel COUDRAY <axel.hpm.coudray@gmail.com>
2020-04-07 17:36:36 +0200

    initialization end

--- App/Config/index.dev.js
+++ App/Config/index.dev.js
@@ -1,4 +1,3 @@
 export const Config = {
-  API_URL: 'https://jsonplaceholder.typicode.com/users/',
-  API_BASE_URL: 'http://localhost:3000/'
+  API_URL: 'https://jsonplaceholder.typicode.com/users/'
 }
